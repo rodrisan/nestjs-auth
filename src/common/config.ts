f3423ee0c8401f82a503d2e366858e07066c9baf
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-30 14:28:22 -0600

    feat(): preparing for deploy on RailWay

--- src/common/config.ts
+++ src/common/config.ts
@@ -9,6 +9,7 @@ export default registerAs('config', () => {
       dbUser: process.env.MYSQL_USER,
       dbPass: process.env.MYSQL_PASS,
     },
+    mysqlUrl: process.env.MYSQL_DATABASE_URL,
     postgres: {
       dbHost: process.env.POSTGRES_HOST,
       dbName: process.env.POSTGRES_DB_NAME,
@@ -16,6 +17,7 @@ export default registerAs('config', () => {
       dbUser: process.env.POSTGRES_USER,
       dbPass: process.env.POSTGRES_PASS,
     },
+    postgresUrl: process.env.POSTGRES_DATABASE_URL,
     allowCors: process.env.ALLOW_CORS,
     apiKey: process.env.API_KEY,
     jwtSecret: process.env.JWT_SECRET,
